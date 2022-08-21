fe5d6c392593d299d864bd00a8d0948f4d837c86
lucasstarsz <andrewrcdey@gmail.com>
2022-08-21 12:15:36 -0400

    fix pausing issues, allow p/escape to unpause

--- src/main/java/tech/fastj/gj/FastJGameJam2022.java
+++ src/main/java/tech/fastj/gj/FastJGameJam2022.java
@@ -1,6 +1,7 @@
 package tech.fastj.gj;
 
 import tech.fastj.engine.FastJEngine;
+import tech.fastj.gameloop.CoreLoopState;
 import tech.fastj.logging.LogLevel;
 
 import javax.swing.UIManager;
@@ -17,6 +18,8 @@ public class FastJGameJam2022 {
         FlatDarkLaf.setup();
         FastJEngine.init(GameName, new GameManager());
         FastJEngine.setTargetUPS(1);
+        FastJEngine.getGameLoop().getGameLoopStates().get(CoreLoopState.FixedUpdate).clear();
+        FastJEngine.getGameLoop().getGameLoopStates().get(CoreLoopState.FixedUpdate).add(FastJEngine.GeneralFixedUpdate);
         FastJEngine.configureLogging(LogLevel.Debug);
 
         try {
